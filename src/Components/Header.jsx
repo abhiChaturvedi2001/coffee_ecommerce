aa7c479db3367bbce8fd4790b989968798be2349
ABHCH <chaturvedia435@gmail.com>
2024-02-10 13:02:52 +0530

    change state of login

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CiUser } from "react-icons/ci";
 import { App_logo } from "../Utils/constant";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../Utils/firebase";
+import { addUser, removeUser } from "../Utils/userSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((store) => store.user);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+          })
+        );
+        navigate("/");
+      } else {
+        dispatch(removeUser());
+        navigate("/login");
+      }
+    });
+  }, []);
+
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
     <>
       <nav className="flex justify-around absolute w-full items-center min-h-[12vh]">
@@ -20,12 +57,20 @@ const Header = () => {
             <li>About us</li>
           </Link>
           <li>Delivery</li>
-          <li>
-            <Link to={"/login"}>
-              {" "}
-              <CiUser className="text-3xl cursor-pointer" />
-            </Link>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to={"/login"}>
+                {" "}
+                <CiUser className="text-3xl cursor-pointer" />
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && <div>Welcome 👋🏻 {isLoggedIn.displayName}</div>}
+          {isLoggedIn && (
+            <div onClick={handleSignout} className="border px-3 py-2 ">
+              Logout
+            </div>
+          )}
         </ul>
         <div className="hidden max-sm:block">X</div>
       </nav>
